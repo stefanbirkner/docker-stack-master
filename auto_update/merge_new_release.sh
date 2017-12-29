0b804d1873cd99cd12329f2fdfc264038beaa123
Stefan Birkner <mail@stefan-birkner.de>
2017-12-29 22:14:08 +0100

    xx

--- auto_update/merge_new_release.sh
+++ auto_update/merge_new_release.sh
@@ -5,10 +5,6 @@ set -e
 if [ "$TRAVIS_BRANCH" = "new_release" ]
 then
   echo "sfb $TRAVIS_COMMIT"
-  git fetch origin master
-  git fetch origin new_release
-  git checkout master
-  git merge --ff-only origin/new_release
-  git push origin master
+  git push origin "$TRAVIS_COMMIT:master"
   git push origin :new_release
 fi
